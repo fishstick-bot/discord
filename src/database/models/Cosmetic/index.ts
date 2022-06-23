8c6ad1d6d7de9a148ed3b218f1ec2e69b7ecba8c
Vanxh <71810662+vanxh@users.noreply.github.com>
2022-06-23 22:37:56 +0530

    format

--- src/database/models/Cosmetic/index.ts
+++ src/database/models/Cosmetic/index.ts
@@ -10,62 +10,60 @@ import {
 } from '../../schemaTypes';
 import { ICosmetic } from './typings';
 
-const CosmeticSchema = new Schema<ICosmetic>(
-  {
-    id: uniqueRequiredString,
-    name: requiredString,
-    description: requiredString,
-    type: {
-      ...requiredObjectId,
-      ref: 'CosmeticType',
-      autopopulate: true,
-    },
-    rarity: {
-      ...requiredObjectId,
-      ref: 'CosmeticRarity',
-      autopopulate: true,
-    },
-    series: {
-      ...objectId,
-      ref: 'CosmeticSeries',
-      autopopulate: true,
-    },
-    set: {
-      ...objectId,
-      ref: 'CosmeticSet',
-      autopopulate: true,
-    },
-    introduction: {
-      ...objectId,
-      ref: 'CosmeticIntroducedIn',
-      autopopulate: true,
-    },
-    image: {
-      type: Buffer,
-      required: true,
-    },
-    searchTags: stringArray,
-    gameplayTags: stringArray,
-    metaTags: stringArray,
-    showcaseVideo: {
-      type: String,
-      required: false,
-    },
-    path: requiredString,
-    addedAt: {
-      type: Date,
-      required: true,
-    },
-    shopHistory: {
-      type: [Date],
-      required: false,
-      default: [],
-    },
+const CosmeticSchema = new Schema<ICosmetic>({
+  id: uniqueRequiredString,
+  name: requiredString,
+  description: requiredString,
+  type: {
+    ...requiredObjectId,
+    ref: 'CosmeticType',
+    autopopulate: true,
   },
-  {
-    timestamps: true,
+  rarity: {
+    ...requiredObjectId,
+    ref: 'CosmeticRarity',
+    autopopulate: true,
   },
-);
+  series: {
+    ...objectId,
+    ref: 'CosmeticSeries',
+    autopopulate: true,
+  },
+  set: {
+    ...objectId,
+    ref: 'CosmeticSet',
+    autopopulate: true,
+  },
+  introduction: {
+    ...objectId,
+    ref: 'CosmeticIntroducedIn',
+    autopopulate: true,
+  },
+  image: {
+    type: Buffer,
+    required: true,
+  },
+  searchTags: stringArray,
+  gameplayTags: stringArray,
+  metaTags: stringArray,
+  showcaseVideo: {
+    type: String,
+    required: false,
+  },
+  path: requiredString,
+  addedAt: {
+    type: Date,
+    required: true,
+  },
+  shopHistory: {
+    type: [Date],
+    required: false,
+    default: [],
+  },
+},
+{
+  timestamps: true,
+});
 CosmeticSchema.plugin(autopopulate);
 
 export default model<ICosmetic>('Cosmetic', CosmeticSchema);
