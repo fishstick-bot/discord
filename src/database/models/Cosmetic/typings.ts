d1ebe74b40489663ecb11ac8ac68eae8f0447e43
Vanxh <71810662+vanxh@users.noreply.github.com>
2022-06-27 00:45:54 +0530

    remove variants cuz lim gave me a better idea for it :)

--- src/database/models/Cosmetic/typings.ts
+++ src/database/models/Cosmetic/typings.ts
@@ -30,14 +30,6 @@ export interface ICosmeticIntroducedIn {
   seasonNumber: number;
 }
 
-export interface ICosmeticVariant {
-  channel: string;
-  type: string;
-  tag: string;
-  name: string;
-  image: string;
-}
-
 export interface ICosmetic {
     id: string;
     name: string;
@@ -48,7 +40,6 @@ export interface ICosmetic {
     set?: Schema.Types.ObjectId | ICosmeticSet;
     introduction?: ICosmeticIntroducedIn;
     image: Buffer;
-    variants?: ICosmeticVariant[];
     searchTags: string[];
     gameplayTags: string[];
     metaTags: string[];
