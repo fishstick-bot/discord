6cab806877dd92b0a65e19367690743745ef1232
Vanxh <71810662+vanxh@users.noreply.github.com>
2022-12-23 15:40:14 +0530

    fix truncate

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -102,8 +102,7 @@ const handleCommandError = async (
 };
 
 const truncateString = (str: string) =>
-  // return string in format "a...z"
-  `${str.substring(0, 1)}${'*'.repeat(str.length - 2)}${str.substring(
+  `${str.substring(0, 1)}${'#'.repeat(str.length - 2)}${str.substring(
     str.length - 1,
     str.length,
   )}`;
